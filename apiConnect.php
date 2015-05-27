ad859b5ea0dcbc7b7c0ac6a8eb803c41d6b7c924
Amanda Fernandez <louri1991@gmail.com>
2015-05-27 16:45:50 +0200

    Conexión a la API, no te olvides de volcar en tu localhost el proyectoHoteles que te he mandado por correo y ejecutar el .sql en tu localhost también

---
+++ apiConnect.php
@@ -0,0 +1,61 @@
+<?php
+function getHoteles(){
+	$url = 'http://localhost/proyectoHoteles/hotel';
+	$curl = curl_init($url);
+
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+	$curl_response = curl_exec($curl);
+
+	if ($curl_response === false) {
+	    $info = curl_getinfo($curl);
+	    curl_close($curl);
+	    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+	}
+
+	curl_close($curl);
+
+	$result = json_decode($curl_response, true);
+	$hoteles = (array)$result;
+
+	if (isset($result->response->status) && $result->response->status == 'ERROR') {
+	    die('error occured: ' . $result->response->errormessage);
+	}
+
+	//Esto nos devuelve un array, no un string, con lo cual no podemos mostrarlo con un echo
+	//Hay que recorrer el array para mostrar todos los hoteles
+	//print_r($hoteles); -> Esto lo imprime por pantalla, para comprobar que funciona nuestra API
+	return $result;
+}
+
+function getImagenes($codigoHotel){
+	$url = 'http://localhost/proyectoHoteles/img?codigoHotel='.$codigoHotel.;
+	$curl = curl_init($url);
+
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+	$curl_response = curl_exec($curl);
+
+	if ($curl_response === false) {
+	    $info = curl_getinfo($curl);
+	    curl_close($curl);
+	    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+	}
+
+	curl_close($curl);
+
+	$result = json_decode($curl_response, true);
+	$hoteles = (array)$result;
+
+	if (isset($result->response->status) && $result->response->status == 'ERROR') {
+	    die('error occured: ' . $result->response->errormessage);
+	}
+
+	//Esto nos devuelve un array, no un string, con lo cual no podemos mostrarlo con un echo
+	//Hay que recorrer el array para mostrar todos los hoteles
+	//print_r($hoteles); -> Esto lo imprime por pantalla, para comprobar que funciona nuestra API
+	return $result;
+}
+
+
+?>
--- contacta/inscripcion.php
+++ contacta/inscripcion.php
@@ -1,3 +1,9 @@
+      <?php include 'apiConnect.php'; 
+      /*
+      Incluimos la conexión a la API;
+      */
+            $hoteles = getHoteles();
+      ?>
       <fieldset class="inscripcion">
          <legend><h2>Datos de inscripción</h2></legend>
          <select class="form-control" style="width:60%">
@@ -7,8 +13,22 @@
          </select>
          <input type="text" class="form-control" placeholder="Nombre" style="width:60%" required>
          <input type="text" class="form-control" placeholder="Apellidos" style="width:80%" required>
+         <input type="text" class="form-control" placeholder="Centro de Trabajo" style="width:80%" required>
+         <input type="text" class="form-control" placeholder="Teléfono" style="width:60%" required>
          <input type="email" class="form-control" placeholder="Dirección de correo" style="width:100%" required>
          <input type="email" class="form-control" placeholder="Confirmar dirección de correo" style="width:100%" required>
+         <input type="text" class="form-control" placeholder="Contraseña" style="width:80%" required>
+         <input type="text" class="form-control" placeholder="Actividades" style="width:80%">
+         <select class="form-control" style="width:80%">
+            <?php foreach ($hoteles as $hotel => $value) {
+                  echo '<option>Hotel: ';
+                  echo $value['nombreHotel'];
+                  //echo $hotel;
+                  ' </option>';
+            }?>
+           
+         </select>
+
          <div style="text-align:center">
             <button type="button" class="btn btn-primary" onClick="javascript:validarContacto(formularioContacto);">Inscribir</button>
          </div>
